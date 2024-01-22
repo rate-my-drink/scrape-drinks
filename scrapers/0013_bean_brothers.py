284ce9f42a25a12d44e80c76a546f49e3eb0eead
wagenrace <nijhof.dns@gmail.com>
2024-01-22 21:21:43 +0100

    scrap bean brothers

--- scrapers/0013_bean_brothers.py
+++ scrapers/0013_bean_brothers.py
@@ -26,27 +26,43 @@ drinks_soup = BeautifulSoup(response.content, "html.parser")
 
 drink_items = drinks_soup.find_all(
     "div",
-    class_="item item-hover-view-cart",
+    class_="product-small box",
 )
 all_drink_items += drink_items
 
 all_drinks: List[Drink] = []
 for drink_item in tqdm(all_drink_items):
-    title = drink_item.find("h4", class_="product-name").text.strip()
-    href = drink_item.find("a")["href"]
-    image_url = drink_item.find("img")["src"]
+    title_div = drink_item.find("div", class_="title-wrapper")
+    title = title_div.text.strip()
+    href = title_div.find("a")["href"]
     new_drink = Drink(
-        href=f"{BASE_URL}{href}", name=title, producer=PRODUCER_ID, image_url=image_url
+        href=href,
+        name=title,
+        producer=PRODUCER_ID,
     )
     all_drinks.append(new_drink)
 
 for drink in tqdm(all_drinks):
-    response = requests.get(drink.href, headers=headers)
+    response = requests.get(drink.href)
     if response.status_code != 200:
-        raise ConnectionError(f"Did not get a 200 status from {drink.href}")
+        raise ConnectionError(
+            f"Got {response.status_code} status from {drink.href}. {response.content}"
+        )
     drink_soup = BeautifulSoup(response.content, "html.parser")
 
-    description = drink_soup.find("div", class_="product-description").text.strip()
+    image_url = drink_soup.find("img", class_="wp-post-image skip-lazy")["src"]
+
+    drink.image_url = image_url
+
+    description_ps = drink_soup.find(
+        "div", class_="product-short-description"
+    ).find_all("p")
+    description = ""
+    for p in description_ps:
+        text = p.text.strip()
+        if text:
+            description += f"{text}\n"
+
     drink.description = description
 
 export_drinks(PRODUCER_NAME, all_drinks)
