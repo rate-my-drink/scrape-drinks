7147ccf34b56477a1652504f008f57c49a6b381b
wagenrace <nijhof.dns@gmail.com>
2024-02-15 21:08:06 +0100

    add local tea

--- upload/upload_all.py
+++ upload/upload_all.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 
 client = SupabaseCC()
 
-csv_loc = Path("upload") / "filtered_data" / "Celestial Seasonings.csv"
+csv_loc = Path("upload") / "filtered_data" / "Local tea.csv"
 
 
 df = pd.read_csv(csv_loc)
